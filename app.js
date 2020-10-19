7f63de32d34ea3eed56284f38b2886b78e1405a8
msanmaz <osanmaz4992@gmail.com>
2020-10-19 09:22:31 +0100

    backend chunks

--- app.js
+++ app.js
@@ -1,19 +1,94 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
+const cors = require('cors');
+const thumbsupply = require('thumbsupply');
 
 const app = express();
+app.use(cors());
 
-app.get('/video', (req, res) => {
-    res.sendFile('assets/videoatti.mp4', { root: __dirname });
+
+const videos = [
+    {
+        id: 0,
+        poster: '/video/0/poster',
+        duration: '3 mins',
+        name: 'IS TOPLASINDA GIZLICE SIKIYOR'
+    },
+    {
+        id: 1,
+        poster: '/video/1/poster',
+        duration: '4 mins',
+        name: 'BUYUK YARRAGIYLA DARACIK AMINI DELIYOR'
+    },
+    {
+        id: 2,
+        poster: '/video/2/poster',
+        duration: '2 mins',
+        name: 'AGZINA ALIP CILDIRIYOR'
+    },
+    {
+        id: 3,
+        poster: '/video/3/poster',
+        duration: '4 mins',
+        name: 'IKISINE BIRDEN ANCA YETIYOR'
+    },
+]
+
+app.get('/video', (req, res, next) => {
+    res.sendFile('assets/0.mp4', { root: __dirname });
 });
 
 
 
+app.get('/videos', (req, res) => res.json(videos));
 
+app.get('/video/:id/data', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    res.json(videos[id]);
+});
+
+
+app.get('/video/:id', (req, res) => {
+    const path = `assets/${req.params.id}.mp4`;
+    const stat = fs.statSync(path);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+    if (range) {
+        const parts = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1]
+            ? parseInt(parts[1], 10)
+            : fileSize-1;
+        const chunksize = (end-start) + 1;
+        const file = fs.createReadStream(path, {start, end});
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(206, head);
+        file.pipe(res);
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(path).pipe(res);
+    }
+});
+
+
+app.get('/video/:id/poster', function(req, res) {
+    thumbsupply.generateThumbnail(`assets/${req.params.id}.mp4`)
+      .then(thumb => res.sendFile(thumb))
+      .catch(err => console.log(err))
+  });
 
 
 
 app.listen(4000, () => {
     console.log('Listening on port 4000!')
 });
+
